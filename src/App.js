8dc33367d9242426a0b865143c4d0e451fa62307
NicoooM <nmarsan33000@gmail.com>
2021-11-25 15:31:51 +0100

    convert timer class to functional component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import { tLyrics } from "./happy.js";
 import Timer from "./components/timer";
+import { useState, useEffect } from "react";
 
 function App() {
   const min = tLyrics[0][0].substring(1, 3);
   const sec = tLyrics[0][0].substring(4, 6);
   const msec = tLyrics[0][0].substring(7, 9);
   const text = tLyrics[0][1];
-  console.log(min);
-  console.log(sec);
-  console.log(msec);
-  console.log(text);
+
+  const [lyrics, setLyrics] = useState(tLyrics[0][1]);
+  // const []
 
   return (
     <div className="App">
       <Timer></Timer>
+      <ul>
+        {tLyrics.map((line, index) => {
+          return <li key={index}>{line[0] + " " + line[1]}</li>;
+        })}
+      </ul>
     </div>
   );
 }
--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -1,35 +1,30 @@
-import React, { Component } from "react";
-class Timer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { seconds: 0 };
+import { useState, useEffect } from "react";
+
+export default function Timer() {
+  const [seconds, setSeconds] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+
+  useEffect(() => {
+    if (isOn) {
+      const interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 0.01);
+      }, 10);
+      return () => clearInterval(interval);
     }
-    onStart = () => {
-        this.setState({ seconds: this.state.seconds + 0.01 });
-    };
-    timer = () => {
-        this.f = setInterval(this.onStart, 10);
-        document.getElementById("btn").disabled = true;
-    };
-    onPause = () => {
-        clearInterval(this.f);
-    };
-    onReset = () => {
-        clearInterval(this.f);
-        document.getElementById("btn").disabled = false;
-        this.setState({ seconds: 0 });
-    };
-    render() {
-        return (
-            <div>
-                <h1>{this.state.seconds.toFixed(2)}</h1>
-                <button id="btn" onClick={this.timer}>
-                    Demarrer
-                </button>
-                <button onClick={this.onPause}>Pause</button>
-                <button onClick={this.onReset}>Reset</button>
-            </div>
-        );
-    }
+  }, [isOn]);
+
+  const onReset = () => {
+    setSeconds(0);
+    setIsOn(false);
+  };
+
+  return (
+    <div>
+      <h1>{seconds.toFixed(2)}</h1>
+      <button id="btn" onClick={() => setIsOn(!isOn)}>
+        {isOn ? "Pause" : "Démarrer"}
+      </button>
+      <button onClick={onReset}>Mettre à 0</button>
+    </div>
+  );
 }
-export default Timer;
