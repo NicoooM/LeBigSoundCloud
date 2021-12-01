ca886716fc0995459b8c71ad6c106f0fa4373ab9
MerlinWasTaken <gaetanjestin@outlook.com>
2021-12-01 15:33:39 +0100

    stylised 404 page, added californication

--- src/pages/Quatrecentquatre/index.js
+++ src/pages/Quatrecentquatre/index.js
@@ -1,11 +1,28 @@
 import { Link } from "react-router-dom";
+import "./quatrecentquatre.scss";
 
 function quatrecentquatre() {
   return (
-    <div>
-      <h1>Dommage ! Mauvaise pioche !</h1>
-      <Link to="/">Homepage</Link>
-    </div>
+    <main>
+      <header className="container">
+        <h1 className="ph-regular">Dang.</h1>
+        <div className="line"></div>
+      </header>
+
+      <div className="center-container">
+        <p className="big-text">404.</p>
+        <div className="even-center-container">
+          <p className="small-text">404.</p>
+        </div>
+      </div>
+
+      <footer className="container">
+        <div className="line"></div>
+        <Link to="/" className="ph-regular">
+          Accueil.
+        </Link>
+      </footer>
+    </main>
   );
 }
 
